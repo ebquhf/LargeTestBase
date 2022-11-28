b5049ee8aa41d27e02ec43c2cfd3e322b142be4e
ebquhf <barna211@vipmail.hu>
2022-11-28 20:36:38 +0100

    Add project files.

---
+++ LargeTest/BikeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Learning.CSharp
+{
+    internal class BikeRepository
+    {
+        public BikeRepository()
+        {
+        }
+
+        public IEnumerable<object> Bikes { get; internal set; }
+    }
+}
---
+++ LargeTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Learning.CSharp;
+using static Learning.CSharp.DelegateTest;
+
+class Program
+{
+    public static event IncrementDelegate MyEvent;
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle { Length = 5, Width = 3 };
+        Console.WriteLine($"The length is {rect.Length} and the width is {rect.Width}");
+        LearnCSharp.OtherInterestingFeatures();
+        // Refer to the Increment method by instantiating the delegate
+        // and passing the method itself in as an argument
+        IncrementDelegate inc = new IncrementDelegate(Increment);
+        Console.WriteLine(inc());  // => 1
+
+        // Delegates can be composed with the + operator
+        IncrementDelegate composedInc = inc;
+        composedInc += inc;
+        composedInc += inc;
+
+        // composedInc will run Increment 3 times
+        Console.WriteLine(composedInc());  // => 4
+
+
+        // Subscribe to the event with the delegate
+        MyEvent += new IncrementDelegate(Increment);
+        MyEvent += new IncrementDelegate(Increment);
+
+        // Trigger the event
+        // ie. run all delegates subscribed to this event
+        Console.WriteLine(MyEvent());  // => 6
+        string username = "User";
+        Console.WriteLine($@"C:\Users\{username}\Desktop");
+        PragmaWarning.ObsoleteMethod(); // CS0618: 'ObsoleteMethod is obsolete: Use NewMethod instead'
+#pragma warning disable CS0618
+        PragmaWarning.ObsoleteMethod(); // CS0618: 'ObsoleteMethod is obsolete: Use NewMethod instead'
+#pragma warning restore CS0618
+        PragmaWarning.ObsoleteMethod(); // CS0618: 'ObsoleteMethod is obsolete: Use NewMethod instead'
+    }
+}
